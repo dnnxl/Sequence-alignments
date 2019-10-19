58965a4f93ff638e26cbb28176a51c70811c5c1a
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-10-19 16:28:18 -0600

    Update tools.py

--- sequence-alignments/src/tools/tools.py
+++ sequence-alignments/src/tools/tools.py
@@ -1,13 +1,31 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Oct 18 09:27:04 2019
-
 @author: dnnxl
 """
+
+# Global values
 values = {"GAP": -2, "MISSMATCH": -1, "MATCH":1}
+distribution = {"UNIFORM": 1, "GAUSSIAN": 2, "POISSON": 3}
+
+# Description:    
+# Input:
+# Output:
+# Author: Danny Xie Li
+# Last modification:
 
 def compare(a, b):
     if(a == b):
         return 1
     else:
         return -1
+    
+# Description:    
+# Input:
+# Output:
+# Author: Danny Xie Li
+# Last modification:
+
+def mapConversion(value, minOld, maxOld, minNew, maxNew):
+    newValue = ( (value - minOld) / (maxOld - minOld) ) * (maxNew - minNew) + minNew
+    return newValue
