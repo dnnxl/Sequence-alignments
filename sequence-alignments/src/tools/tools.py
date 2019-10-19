df7876d84de5f44014e03da3bb9d5b2373147428
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-10-18 23:08:03 -0600

    Update tools.py

--- sequence-alignments/src/tools/tools.py
+++ sequence-alignments/src/tools/tools.py
@@ -1,1 +1,13 @@
-# Functions
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 09:27:04 2019
+
+@author: dnnxl
+"""
+values = {"GAP": -2, "MISSMATCH": -1, "MATCH":1}
+
+def compare(a, b):
+    if(a == b):
+        return 1
+    else:
+        return -1
