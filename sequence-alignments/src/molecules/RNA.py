c88223c03b15144888e5293abace03a9fca07a00
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-10-18 23:15:47 -0600

    Update RNA.py

--- sequence-alignments/src/molecules/RNA.py
+++ sequence-alignments/src/molecules/RNA.py
@@ -1,1 +1,19 @@
 # RNA class
+class RNA:
+  def __init__(self, molecule):
+    self.molecule = molecule
+  
+  def isRNA(self):
+    value = True
+    for i in range(0, len(self.molecule)):
+      if(self.molecule[i] == "A" or self.molecule[i] == "U" or self.molecule[i] == "C" or self.molecule[i] == "G"):
+        value = value and True
+      else:
+        return False
+    return True
+  
+  def getMolecule(self):
+    return self.molecule
+  
+  def setMolecule(self, molecule):
+    self.molecule = molecule
