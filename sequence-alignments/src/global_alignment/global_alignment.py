982c72ac4ecb91b1a4ecfe69263a1336844659f6
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-10-18 23:07:31 -0600

    Update global_alignment.py

--- sequence-alignments/src/global_alignment/global_alignment.py
+++ sequence-alignments/src/global_alignment/global_alignment.py
@@ -1,25 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep 29 18:29:27 2019
+Created on Fri Oct 18 15:01:43 2019
 
 @author: dnnxl
 """
+
 import numpy as np
+import tools as tls
 
-values = {"GAP": -2, "MISSMATCH": -1, "MATCH":1}
-
-def compare(a, b):
-    if(a == b):
-        return 1
-    else:
-        return 0
-
-class globalAligment():
+class NeedlemanWunsch:
     
-    def __init__(self, sequenceS, sequenceT, penalty):
+    def __init__(self, sequenceS, sequenceT):
         self.sequenceS = sequenceS
         self.sequenceT = sequenceT
-        self.penalty = penalty
         self.matrix = np.zeros((len(sequenceS)+1, len(sequenceT)+1))
         
     def getSequenceS(self):
@@ -50,60 +43,15 @@ class globalAligment():
         sizeM = len(self.sequenceS)
         sizeN = len(self.sequenceT)
         for i in range(0, sizeM+1):
-            self.matrix[i, 0] = i*self.penalty
+            self.matrix[i, 0] = i*values["GAP"]
         
         for j in range(0, sizeN+1):
-            self.matrix[0, j] = j*self.penalty
+            self.matrix[0, j] = j*values["GAP"]
 
         for i in range(1, sizeM+1):
             for j in range(1, sizeN+1):    
-                self.matrix[i, j] = max(self.matrix[i-1, j] + self.penalty, self.matrix[i-1, j-1] + compare(self.sequenceS[i-1], self.sequenceT[j-1]), self.matrix[i, j-1] + self.penalty)
+                self.matrix[i, j] = max(self.matrix[i-1, j] + values["GAP"], self.matrix[i-1, j-1] + tls.compare(self.sequenceS[i-1], self.sequenceT[j-1]), self.matrix[i, j-1] + values["GAP"])
         return self.matrix
-    
-    
-    
-    def align(self):
-      sequenceS = self.sequenceS
-      sequenceT = self.sequenceT 
-
-      alignS = ""
-      alignT = ""
-
-      lenS = len(sequenceS)
-      lenT = len(sequenceT)
-
-      while(lenS >= 0 and lenT >=0):
-        if(j-1 < 0 and i-1 < 0):
-          break
-      if(i-1 < 0):
-        scoreUp = None
-        scoreDiag = None
-      if(j-1<0):
-        scoreLeft = None
-        scoreDiag = None
-
-      score = self.matrix[lenS, lenT]
-      scoreDiag = self.matrix[lenS-1, lenT-1]
-      scoreUp = self.matrix[lenS-1, lenT]
-      scoreLeft = self.matrix[lenS, lenT-1]
-
-      if(scoreDiag != None and score == scoreDiag+ compare(sequenceS[lenS-1], sequenceT[lenT-1])):
-        alignS = sequenceS[lenS-1] + alignS
-        alignT = sequenceT[lenT-1] + alignT
-        lenS = lenS-1
-        lenT = lenT-1
-        continue
-
-      if(scoreLeft != None and score == scoreLeft + values["GAP"]):
-        alignS = "-" + alignS
-        alignT = sequenceT[lenT-1]+ alignT
-        lenT = lenT-1
-        continue
-      else:
-        alignS = sequenceS[lenS-1] + alignS
-        alignT = "-" + alignT
-        lenS = lenS-1
-        continue
     
         
     def displayResults(self):
@@ -122,8 +70,49 @@ class globalAligment():
         print("First alignment ")
         print(" pppp ")
         print("Second aligment ")
-    
 
-    
-n = globalAligment("ABC", "ABC", -2)
+    def align(self):
+      sequenceS = self.sequenceS
+      sequenceT = self.sequenceT 
+
+      alignS = ""
+      alignT = ""
+
+      lenS = len(sequenceS)
+      lenT = len(sequenceT)
+
+      while(lenS >= 0 and lenT >=0):
+          if(lenT-1 < 0 and lenS-1 < 0):
+              break
+          if(lenS-1 < 0):
+              scoreUp = None
+              scoreDiag = None
+          if(lenT-1<0):
+              scoreLeft = None
+              scoreDiag = None
+          score = self.matrix[lenS, lenT]
+          scoreDiag = self.matrix[lenS-1, lenT-1]
+          scoreUp = self.matrix[lenS-1, lenT]
+          scoreLeft = self.matrix[lenS, lenT-1]
+
+          if(scoreDiag != None and score == scoreDiag+ tls.compare(sequenceS[lenS-1], sequenceT[lenT-1])):
+              alignS = sequenceS[lenS-1] + alignS
+              alignT = sequenceT[lenT-1] + alignT
+              lenS = lenS-1
+              lenT = lenT-1
+              continue
+          if(scoreLeft != None and score == scoreLeft + values["GAP"]):
+              alignS = "-" + alignS
+              alignT = sequenceT[lenT-1]+ alignT
+              lenT = lenT-1
+              continue
+          else:
+              alignS = sequenceS[lenS-1] + alignS
+              alignT = "-" + alignT
+              lenS = lenS-1
+              continue
+      return (alignS, alignT)
+        
+n = NeedlemanWunsch("BC", "ABC")
 print(n.similarity())
+print(n.align())
