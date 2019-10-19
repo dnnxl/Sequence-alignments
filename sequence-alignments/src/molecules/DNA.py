c7c73be79b9e344295d5eddab4e630de1201500c
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-10-18 23:14:55 -0600

    Update DNA.py

--- sequence-alignments/src/molecules/DNA.py
+++ sequence-alignments/src/molecules/DNA.py
@@ -6,7 +6,7 @@ class DNA:
   def isDNA(self):
     value = True
     for i in range(0, len(self.molecule)):
-      if(self.molecule[i] == "A" or self.molecule[i] == "G" or self.molecule[i] == "C" or self.molecule[i] == "T" or):
+      if(self.molecule[i] == "A" or self.molecule[i] == "G" or self.molecule[i] == "C" or self.molecule[i] == "T"):
         value = value and True
       else:
         return False
